ebcfb6e8b907890d13815b75c38c12c3546b6e09
mosquerasara <mosquerasara2106@gmail.com>
2026-05-15 13:41:42 -0500

    Actualicé el home, puse un menú y agregué nuevas paginas

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def inicio():
     return render_template("inicio.html")
+
+@app.route("/reservas")
+def reservas():
+    return render_template("reserva.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
+@app.route("/nosotros")
+def nosotros():
+    return render_template("nosotros.html")
+
+@app.route("/mar")
+def mar():
+    return render_template("mar.html")
+
+@app.route("/tierra")
+def tierra():
+    return render_template("tierra.html")
+
+@app.route("/especialidades")
+def especialidades():
+    return render_template("especialidades.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
